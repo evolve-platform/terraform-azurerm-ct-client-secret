f214438a2e0c88297524c46e7ec52e6c069b072c
Joost Smit <j.smit@labdigital.nl>
2025-08-22 18:48:27 +0200

    fix: update versions to not reference aws

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.11"
     }
     commercetools = {
       source = "labd/commercetools"
